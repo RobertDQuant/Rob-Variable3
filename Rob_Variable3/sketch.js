8d83d746ea07e4ec9a49ea2179e7491218884724
RobertDQuant <Robertdquant@gmail.com>
2017-02-24 15:02:38 -0500

    Update sketch.js

--- Rob_Variable3/sketch.js
+++ Rob_Variable3/sketch.js
@@ -1,19 +1,21 @@
-var x = 0;
+//these are all variables
+// this is the position of the shape
+var x = 0; 
 var y = 200;
-var diameter = 50;
-var r = 23;
+var diameter = 50; // this is the size of the shape
+var r = 23; //this is the color of the shape
 var g = 160;
 var b = 135;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 400); //this is the size of the canvas
 }
 
 function draw() {
-  background(r, g, b);
-  fill(250, 200, 200);
+  background(r, g, b); //this is the color of the background
+  fill(250, 200, 200); //this is the color of the shape
   ellipse(x, y, diameter, diameter);
   
   x = x + 1;
-  y = y + 0.3;
+  y = y + 0.3; // this makes the shape move at a angle
 }
